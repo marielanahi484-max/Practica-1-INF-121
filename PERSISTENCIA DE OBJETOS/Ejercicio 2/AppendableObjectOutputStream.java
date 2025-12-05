7154e8c69b2505d148677edd639f58d0d8bc9e34
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 19:04:25 -0400

    Create AppendableObjectOutputStream.java

---
+++ PERSISTENCIA DE OBJETOS/Ejercicio 2/AppendableObjectOutputStream.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class AppendableObjectOutputStream extends ObjectOutputStream {
+    public AppendableObjectOutputStream(OutputStream out) throws IOException {
+        super(out);
+    }
+    @Override
+    protected void writeStreamHeader() throws IOException {
+        reset();
+    }
+}
