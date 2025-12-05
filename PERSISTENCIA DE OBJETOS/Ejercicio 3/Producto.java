45b4da30bab17175d9f6b257250ead5b95da365f
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 19:14:12 -0400

    Create Producto.java

---
+++ PERSISTENCIA DE OBJETOS/Ejercicio 3/Producto.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+
+public class Producto implements Serializable {
+    private int codigo;
+    private String nombre;
+    private float precio;
+
+    public Producto(int codigo, String nombre, float precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public String toString() {
+        return "Codigo: " + codigo + " | Nombre: " + nombre + " | Precio: " + precio;
+    }
+ }
