2b572c2455b4eceed9fb1efe5f40c9299d1680d3
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:21:30 -0400

    Create Empleado.java

---
+++ COMPOSICION-AGREGACION/Ejercicio 14/Empleado.java
@@ -0,0 +1,26 @@
+package Ejercicio14;
+
+public class Empleado {
+    private String nombre;
+    private String puesto;
+    private double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Puesto: " + puesto + ", Salario: " + salario;
+    }
+}
