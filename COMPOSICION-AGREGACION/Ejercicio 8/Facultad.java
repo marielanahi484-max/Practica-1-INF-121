6e0dd958c5d52d47a950cb36313b2a8a4790e343
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:17:02 -0400

    Create Facultad.java

---
+++ COMPOSICION-AGREGACION/Ejercicio 8/Facultad.java
@@ -0,0 +1,17 @@
+package Ejercicio8;
+
+public class Facultad {
+	private String nombre;
+	
+	public Facultad(String nombre) {
+		this.nombre=nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	@Override
+	public String toString() {
+		return "Facultad [nombre=" + nombre + "]";
+	}
+	
+}
