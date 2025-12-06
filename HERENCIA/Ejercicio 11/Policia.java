3fdacce20c5041bb01b61608368c5bdbb24e1f2b
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:32:06 -0400

    Create Policia.java

---
+++ HERENCIA/Ejercicio 11/Policia.java
@@ -0,0 +1,19 @@
+package Herencia.Ejercicio11;
+
+public class Policia {
+    private String grado;
+    private String rango;
+
+    public Policia(String grado, String rango) {
+        this.grado = grado;
+        this.rango = rango;
+    }
+
+    public String getGrado() {
+        return grado;
+    }
+
+    public String getRango() {
+        return rango;
+    }
+}
