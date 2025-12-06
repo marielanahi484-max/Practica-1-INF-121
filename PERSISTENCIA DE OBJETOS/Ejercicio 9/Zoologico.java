e2b196697c39021c117f2c842d13958caa963770
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 22:25:25 -0400

    Update Zoologico.java

--- PERSISTENCIA DE OBJETOS/Ejercicio 9/Zoologico.java
+++ PERSISTENCIA DE OBJETOS/Ejercicio 9/Zoologico.java
@@ -4,6 +4,8 @@ public class Zoologico {
     private int nroAnimales;
     private Animal animales[] = new Animal[30];
 
+    public Zoologico() {}
+
     public Zoologico(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
@@ -13,27 +15,30 @@ public class Zoologico {
     public int getId() { return id; }
     public String getNombre() { return nombre; }
     public int getNroAnimales() { return nroAnimales; }
+
     public Animal[] getAnimales() { return animales; }
 
-    public void agregar(Animal a) {
-        if (nroAnimales < 30) {
+    public void agregarAnimal(Animal a) {
+        if(nroAnimales < 30){
             animales[nroAnimales] = a;
             nroAnimales++;
         }
     }
 
-    public void eliminarAnimal(String nombre) {
-        for (int i = 0; i < nroAnimales; i++) {
-            if (animales[i].getNombre().equalsIgnoreCase(nombre)) {
-                animales[i] = animales[nroAnimales - 1];
-                nroAnimales--;
-            }
-        }
+    public boolean estaVacio() {
+        return nroAnimales == 0;
     }
 
-    public int contarEspecies() {
+    public int getVariedad() {
         int cont = 0;
-        for (int i = 0; i < nroAnimales; i++) cont++;
+        for (int i = 0; i < nroAnimales; i++) {
+            cont++;
+        }
         return cont;
+    }
+
+    @Override
+    public String toString() {
+        return "Zoologico{ id=" + id + ", nombre=" + nombre + ", nroAnimales=" + nroAnimales + " }";
     }
 }
