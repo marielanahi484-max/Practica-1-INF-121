055923e248839df22ad42261c4384ee574a65abb
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 22:27:09 -0400

    Update Main9.java

--- PERSISTENCIA DE OBJETOS/Ejercicio 9/Main9.java
+++ PERSISTENCIA DE OBJETOS/Ejercicio 9/Main9.java
@@ -1,103 +1,32 @@
-import java.util.Scanner;
-
 public class Main9 {
     public static void main(String[] args) {
-        ArchZoo arch = new ArchZoo("zoologicos.dat");
-        Scanner sc = new Scanner(System.in);
-        int op;
 
-        do {
-            System.out.println("\n===== MENÚ ZOOLÓGICOS =====");
-            System.out.println("1. Crear zoológico (interactivo)");
-            System.out.println("2. Modificar zoológico por ID");
-            System.out.println("3. Eliminar zoológico por ID");
-            System.out.println("4. Listar zoológicos");
-            System.out.println("5. Listar zoológicos con mayor variedad de animales");
-            System.out.println("6. Listar y eliminar zoológicos vacíos");
-            System.out.println("7. Mostrar animales de especie X");
-            System.out.println("8. Mover animales de un zoológico X a Y");
-            System.out.println("0. Salir");
-            System.out.print("Elija opción: ");
+        ArchZoo arch = new ArchZoo();
 
-            // proteger contra inputs no numéricos
-            while (!sc.hasNextInt()) {
-                System.out.print("Ingrese un número válido: ");
-                sc.next();
-            }
-            op = sc.nextInt();
-            sc.nextLine(); // consumir el salto de línea pendiente
+   
+        Zoologico z1 = new Zoologico(1, "Zoo La Paz");
+        Zoologico z2 = new Zoologico(2, "Zoo Santa Cruz");
 
-            switch (op) {
-                case 1:
-                    // método crear() interactivo en ArchZoo
-                    arch.crear();
-                    break;
+        arch.crear(z1);
+        arch.crear(z2);
 
-                case 2:
-                    System.out.print("ID a modificar: ");
-                    while (!sc.hasNextInt()) {
-                        System.out.print("Ingrese un ID válido: ");
-                        sc.next();
-                    }
-                    int idm = sc.nextInt();
-                    sc.nextLine();
-                    arch.modificar(idm);
-                    break;
+        z1.agregarAnimal(new Animal("Tigre", "Bengala", 2));
+        z1.agregarAnimal(new Animal("Mono", "Capuchino", 4));
 
-                case 3:
-                    System.out.print("ID a eliminar: ");
-                    while (!sc.hasNextInt()) {
-                        System.out.print("Ingrese un ID válido: ");
-                        sc.next();
-                    }
-                    int ide = sc.nextInt();
-                    sc.nextLine();
-                    arch.eliminar(ide);
-                    break;
+        z2.agregarAnimal(new Animal("Tigre", "Siberiano", 1));
 
-                case 4:
-                    arch.listar();
-                    break;
+        System.out.println("Listado inicial:");
+        arch.mostrarTodo();
 
-                case 5:
-                    arch.listarMayorVariedad();
-                    break;
+        arch.listarMayorVariedad();
 
-                case 6:
-                    arch.listarEliminarVacios();
-                    break;
+        arch.mostrarEspecie("tigre");
 
-                case 7:
-                    System.out.print("Ingrese especie a buscar: ");
-                    String esp = sc.nextLine();
-                    arch.mostrarPorEspecie(esp);
-                    break;
+        arch.moverAnimales(1, 2);
 
-                case 8:
-                    System.out.print("Mover del Zoo ID: ");
-                    while (!sc.hasNextInt()) {
-                        System.out.print("Ingrese un ID válido: ");
-                        sc.next();
-                    }
-                    int x = sc.nextInt();
-                    System.out.print("Al Zoo ID: ");
-                    while (!sc.hasNextInt()) {
-                        System.out.print("Ingrese un ID válido: ");
-                        sc.next();
-                    }
-                    int y = sc.nextInt();
-                    sc.nextLine();
-                    arch.moverAnimales(x, y);
-                    break;
+        System.out.println("\nLuego de mover:");
+        arch.mostrarTodo();
 
-                case 0:
-                    System.out.println("Saliendo...");
-                    break;
-
-                default:
-                    System.out.println("Opción inválida!");
-            }
-
-        } while (op != 0);
+        arch.eliminarVacios();
     }
 }
