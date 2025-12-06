e4895bac0d51707b5843f2d352776c7cb8c7d9b5
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:20:16 -0400

    Create Doctor.java

---
+++ COMPOSICION-AGREGACION/Ejercicio 12/Doctor.java
@@ -0,0 +1,29 @@
+package Ejercicio12;
+
+class Doctor {
+    private String nombre;
+    private String especialidad;
+
+    public Doctor(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + especialidad + ")";
+    }
+}
+
+
+
+
+
