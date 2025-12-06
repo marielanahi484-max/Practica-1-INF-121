74186aa55399d8a0caf6e1b2fb114905e778cd14
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:29:42 -0400

    Create Partido.java

---
+++ HERENCIA/Ejercicio 9/Partido.java
@@ -0,0 +1,18 @@
+package Herencia.Ejercicio9;
+public class Partido {
+    private String nombreP;
+    private String rol;
+
+    public Partido(String nombreP, String rol) {
+        this.nombreP = nombreP;
+        this.rol = rol;
+    }
+
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+}
