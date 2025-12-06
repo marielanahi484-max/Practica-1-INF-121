8c4c6b1f2511cc6e71496b58bf3f3489393a1473
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:31:13 -0400

    Create IComparacion.java

---
+++ HERENCIA/Ejercicio 11/IComparacion.java
@@ -0,0 +1,6 @@
+package Herencia.Ejercicio11;
+
+public interface IComparacion {
+    JefePolicia compararSueldo(JefePolicia j1, JefePolicia j2);
+    boolean compararGrado(JefePolicia j1, JefePolicia j2);
+}
