6cc5a0d171879e7d78334523506ed06f38d28358
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:25:20 -0400

    Create Animal.java

---
+++ HERENCIA/Ejercicio 3/Animal.java
@@ -0,0 +1,10 @@
+package Herencia.Ejercicio3;
+
+class Animal {
+    String nombre;
+    int edad;
+
+    public void desplazarse() {
+        System.out.println("El animal se desplaza.");
+    }
+}
