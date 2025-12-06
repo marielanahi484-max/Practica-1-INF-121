ac7aec86f327b5734e0007b192584b78286ac34c
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:35:54 -0400

    Create Main.java

---
+++ HERENCIA/Ejercicio 15/Main.java
@@ -0,0 +1,11 @@
+package Ejercicio15;
+
+public class Main {
+    public static void main(String[] args) {
+        Triatleta t1 = new Triatleta("Crol", "Montaña", 10);
+
+        t1.nadar();
+        t1.pedalear();
+        t1.correr();
+    }
+}
