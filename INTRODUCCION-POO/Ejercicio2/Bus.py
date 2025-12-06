2c8ea2ddbd4cd8f8a2b19aed8660a4bf8416f1a3
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:00:53 -0400

    Create Bus.py

---
+++ INTRODUCCION-POO/Ejercicio2/Bus.py
@@ -0,0 +1,27 @@
+class Bus:
+    def __init__(self, asientos):
+        self.asientos = asientos
+        self.pasajeros = 0
+        self.costo = 1.50
+
+    def subir_pasajeros(self, cantidad):
+        if cantidad <= (self.asientos - self.pasajeros):
+            self.pasajeros += cantidad
+            print(f"{cantidad} pasajeros subieron al bus.")
+        else:
+            print("No hay suficientes asientos disponibles.")
+
+    def cobrar_pasaje(self):
+        total = self.pasajeros * self.costo
+        print(f"El costo total del pasaje es: {total}")
+
+    
+    def mostrar_asientos_disponibles(self):
+        print(f"Asientos disponibles: {self.asientos - self.pasajeros}")
+
+
+if __name__ == "__main__":
+    bus = Bus(40)
+    bus.subir_pasajeros(17)
+    bus.mostrar_asientos_disponibles()
+    bus.cobrar_pasaje()
