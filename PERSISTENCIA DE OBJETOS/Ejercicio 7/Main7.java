3e740cb2b251869d01d2b7a928d1bfb9b6efb3c5
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 21:35:21 -0400

    Create Main7.java

---
+++ PERSISTENCIA DE OBJETOS/Ejercicio 7/Main7.java
@@ -0,0 +1,13 @@
+public class Main7 {
+    public static void main(String[] args) {
+        ArchNiño archivo = new ArchNiño();
+
+        archivo.crear();
+        archivo.listar();
+        archivo.contarPesoAdecuado();
+        archivo.mostrarNoAdecuados();
+        archivo.promedioEdad();
+        archivo.buscar();
+        archivo.mostrarMayorTalla();
+    }
+}
