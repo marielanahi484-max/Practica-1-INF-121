53e8f110b37ffe13cf79818581722171dda6d54d
marielanahi484-max <marielanahi484@gmail.com>
2025-12-05 23:23:59 -0400

    Create Persona.java

---
+++ HERENCIA/Ejercicio 1/Persona.java
@@ -0,0 +1,11 @@
+package Herencia.Ejercicio1;
+
+class Persona {
+    String nombre;
+    String apellido;
+    String ci;
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre + " " + apellido + ", CI: " + ci);
+    }
+}
